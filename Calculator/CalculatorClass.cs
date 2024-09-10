1ea81da3faccd1bd28b44cac5c6cd12588a893b7
dexter2396 <dexterlaum@gmail.com>
2024-09-10 13:11:07 +0800

    Add files via upload

---
+++ Calculator/CalculatorClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+ 
+        public delegate T Formula<T>(T x, T y);
+
+    public class CalculatorClass
+    {
+        public Formula<double> formula;
+        public event Formula<double> CalculateEvent;
+
+        public double GetSum(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double GetDiff(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double GetProd(double x, double y)
+        {
+            return x * y;
+
+        }
+
+        public double GetQout(double x, double y)
+        {
+            return x / y;
+
+        }
+
+        public event Formula<double> calculateEvent
+        {
+            add
+            {
+                Console.WriteLine("Added the Delegate");
+                
+            }
+
+            remove
+            {
+                Console.WriteLine("Remove the Delegate");
+                
+            }
+        }
+
+
+        
+    }
+}
---
+++ Calculator/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+        CalculatorClass cal = new CalculatorClass();
+         double num1;
+         double num2;
+        public Form1()
+        {
+            InitializeComponent();
+            cal = new CalculatorClass();
+        }
+
+        private void cbOperator_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+               
+private void btnEqual_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToDouble(txtBoxInput1.Text);
+            num2 = Convert.ToDouble(txtBoxInput2.Text);
+
+            string selectOperator = cbOperator.SelectedItem.ToString();
+            double result = 0;
+
+            switch (selectOperator)
+            {
+                case "+":
+                    //result = cal.GetSum(num1, num2);
+                    cal.calculateEvent += new Formula<double>(cal.GetSum);
+                    lblDisplayTotal.Text = cal.GetSum(num1, num2).ToString();
+                    cal.calculateEvent -= new Formula<double> (cal.GetSum);
+                    break;
+
+                case "-":
+                    // result = cal.GetDiff(num1, num2);
+                    cal.calculateEvent += new Formula<double>(cal.GetDiff);
+                    lblDisplayTotal.Text = cal.GetDiff(num1, num2).ToString();
+                    cal.calculateEvent -= new Formula<double>(cal.GetDiff);
+                    break;
+
+                case "*":
+                    // result = cal.GetProd(num1, num2);
+                    cal.calculateEvent += new Formula<double>(cal.GetProd);
+                    lblDisplayTotal.Text = cal.GetProd(num1, num2).ToString();
+                    cal.calculateEvent -= new Formula<double>(cal.GetProd);
+                    break;
+
+                case "/":
+                    // result = cal.GetQout(num1, num2);
+                    cal.calculateEvent += new Formula<double>(cal.GetQout);
+                    lblDisplayTotal.Text = cal.GetQout(num1, num2).ToString();
+                    cal.calculateEvent -= new Formula<double>(cal.GetQout);
+                    break;
+
+                default:
+                    return;
+            }
+
+           // lblDisplayTotal.Text = result.ToString();
+    }
+
+        
+    }
+}
